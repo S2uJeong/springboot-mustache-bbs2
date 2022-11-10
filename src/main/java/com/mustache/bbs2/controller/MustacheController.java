f336af89bbf01f11d34c3f0a01a40ed2d7c98e1e
S2ujeong <sj980204@gmail.com>
2022-11-10 13:30:23 +0900

    [Update] 오타 error 수정

--- src/main/java/com/mustache/bbs2/controller/MustacheController.java
+++ src/main/java/com/mustache/bbs2/controller/MustacheController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-// Controller : Model이 데이터를 어떻게 처리할지 알려주는 역할
-//              제공하고자 하는 서비스에 따라 다른 컨트롤러를 생성해서 만들어 주면 유지보수에 좋음
 @Controller
 public class MustacheController {
 
